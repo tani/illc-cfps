8f8121dd614ac32166bd1d54dcc203bd40ac67e4
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-04-18 05:28:18 +0000

    Done!

--- mod.ts
+++ mod.ts
@@ -1,44 +1,87 @@
 /// <reference path="./deployctl.d.ts" />
 
-import * as ics from "https://esm.sh/ics@2.27.0"
+import * as ics from "https://esm.sh/ics@2.27.0";
 import { DateTime } from "https://esm.sh/luxon@1.26.0";
-import * as cheerio from "https://esm.sh/cheerio@1.0.0-rc.6"
+import { cheerio } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
+import * as dejs from "https://deno.land/x/dejs@0.9.3/mod.ts";
 
-const res = await fetch("https://www.illc.uva.nl/NewsandEvents/Events/Conferences/")
-const $ = cheerio.load(await res.text())
-const events = $("#pagecontents > div > section > section:nth-child(9) .vevent").map((_, li) => {
-    const title = $("h4", li).text()
-    const uid = "taniguchi.masaya" + $(".uid", li).attr("href")
-    const _start = DateTime.fromFormat($(".dtstart", li).attr("title") || "", "yyyy-MM-dd")
-    const _end = DateTime.fromFormat($(".dtend", li).attr("title") || "", "yyyy-MM-dd")
-    const location = $(".location", li).text().replace(/Location:\s*/, '')
-    const deadlineSrc = 
-        $("div", li)
-            .filter((_, div)=>$(div).text().search(/Deadline:/) >= 0)
-            .first()
-            .text()
-            .replace(/Deadline:\s*[a-zA-Z]+\s*/, '')
-    const _deadline = DateTime.fromFormat(deadlineSrc, "d MMMM yyyy").setZone('Asia/Tokyo')
-    const start = _deadline.toFormat("yyyy-M-d-H-m").split("-")
-    const description = $(".description", li).text()
+async function GenerateCalendar() {
+  const res = await fetch(
+    "https://www.illc.uva.nl/NewsandEvents/Events/Conferences/",
+  );
+  const $ = cheerio.load(await res.text());
+  const events = $(
+    "#pagecontents > div > section > section:nth-child(9) .vevent",
+  ).map((_, li) => {
+    const title = $("h4", li).text();
+    const uid = "taniguchi.masaya" + $(".uid", li).attr("href");
+    const _start = DateTime.fromFormat(
+      $(".dtstart", li).attr("title") || "",
+      "yyyy-MM-dd",
+    );
+    const _end = DateTime.fromFormat(
+      $(".dtend", li).attr("title") || "",
+      "yyyy-MM-dd",
+    );
+    const location = $(".location", li).text().replace(/Location:\s*/, "");
+    const deadlineSrc = $("div", li)
+      .filter((_, div) => $(div).text().search(/Deadline:/) >= 0)
+      .first()
+      .text()
+      .replace(/Deadline:\s*[a-zA-Z]+\s*/, "");
+    const _deadline = DateTime.fromFormat(deadlineSrc, "d MMMM yyyy").setZone(
+      "Asia/Tokyo",
+    );
+    const start = _deadline.toFormat("yyyy-M-d-H-m").split("-");
+    const description = $(".description", li).text();
     return {
-        _start: _start.isValid ? _start.toFormat("d MMMM yyyyy") : "",
-        _end: _end.isValid ? _end.toFormat("d MMMM yyyy") : "",
-        _deadline: _deadline.isValid ? _deadline.toFormat("d MMMM yyyy") : "",
-        start,
-        duration: { hours: 24 },
-        uid,
-        title,
-        description,
-        location,
-    }
-}).toArray()
+      _start: _start.isValid ? _start.toFormat("d MMMM yyyyy") : "",
+      _end: _end.isValid ? _end.toFormat("d MMMM yyyy") : "",
+      _deadline: _deadline.isValid ? _deadline.toFormat("d MMMM yyyy") : "",
+      start,
+      duration: { hours: 24 },
+      uid,
+      title,
+      description,
+      location,
+    };
+  }).toArray();
+  const template = await (await fetch(new URL("index.ejs", import.meta.url)))
+    .text();
+  const html = await dejs.renderToString(template, { events });
+  events.forEach((event: any) => {
+    delete event._start;
+    delete event._end;
+    delete event._deadline;
+  });
+  return { html, ...ics.createEvents(events as any) };
+}
 
-events.forEach((event: any) => {
-    delete event._start
-    delete event._end
-    delete event._deadline
-})
-
-const deadlines = ics.createEvents(events as any)
-console.log(deadlines)
+self.addEventListener("fetch", async (event) => {
+  const result = await GenerateCalendar();
+  if (new URL(event.request.url).pathname.startsWith("/deadlines.ics")) {
+    const body = result.value;
+    event.respondWith(
+      new Response(
+        body,
+        {
+          headers: {
+            "Content-Type": "text/calendar",
+          },
+        },
+      ),
+    );
+  } else {
+    const body = result.html;
+    event.respondWith(
+      new Response(
+        body,
+        {
+          headers: {
+            "Content-Type": "text/html",
+          },
+        },
+      ),
+    );
+  }
+});
